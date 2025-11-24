8ef6770b313411d0395c6f7818f1d1596e29e531
Acsbeni <acsbenjamin@turr.hu>
2025-11-24 12:23:52 +0100

    Kész

--- modules/torpek.js
+++ modules/torpek.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 //Törpe hozzáadás Form
 router.get('/new', (req, res) => {
     
-    ejs.renderFile('./views/torpe-form.ejs', {session: req.session}, (err, html) => {
+    ejs.renderFile('./views/torpe-new.ejs', {session: req.session}, (err, html) => {
     if (err) {
       res.status(500).send('Error rendering page');
     } else {
@@ -62,5 +62,72 @@ router.post('/new', (req, res) => {
     });
 });
 //Törpe törlés
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    ejs.renderFile(
+        './views/torpe-delete.ejs',
+        { session: req.session, id: id },  
+        (err, html) => {
+            if (err) {
+                res.status(500).send('Error rendering page: ' + err);
+            } else {
+                req.session.error = '';
+                res.send(html);
+            }
+        }
+    );
+});
+//Törpe törlése
+router.post('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query('DELETE FROM torpek WHERE id=?', [id], (err, result) => {
+        if (err) {
+            req.session.error = 'Hiba történt törlésnél!'
+            req.session.severity = 'danger'
+            return res.redirect('/');
+        }
+        res.redirect('/');
+    });
+});
+//Törpe módosítás Form
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('SELECT * FROM torpek WHERE id=?', [id], (err, results) => {
+        if (err || results.length === 0) {
+            req.session.error = 'Hiba történt a betöltésnél!'
+            req.session.severity = 'danger'
+            return res.redirect('/');
+        }
+        ejs.renderFile('./views/torpe-edit.ejs', { session: req.session, id: id ,data: results[0]  }, (err, html) => {
+            if (err) {
+                res.status(500).send('Error rendering page');
+            } else {
+                req.session.error = '';
+                res.send(html);
+            }
+        });
+    });
+});
+//Törpe módosítás POST
+router.post('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const { nev, klan, nem, suly, magassag } = req.body;
+
+    db.query(
+        'UPDATE torpek SET nev=?, klan=?, nem=?, suly=?, magassag=? WHERE id=?',
+        [nev, klan, nem, suly, magassag, id],
+        (err, result) => {
+            if (err) {
+                req.session.error = 'Hiba történt mentésnél!';
+                req.session.severity = 'danger';
+                return res.redirect(`/edit/${id}`);
+            }
+            res.redirect('/');
+        }
+    );
+});
+    
 
 module.exports = router;
