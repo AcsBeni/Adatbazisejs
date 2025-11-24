1f596fbddc5334f0c4cddffd184d71f9455ddc66
Acsbeni <acsbenjamin@turr.hu>
2025-11-24 11:35:02 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+var session = require('express-session')
 const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,9 +10,14 @@ const routes = require('./modules/torpek');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/assets', express.static('assets'));
+app.use(session({
+    secret  : process.env.SESSION_SECRET
+}));
 //Routes
 app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
--- modules/torpek.js
+++ modules/torpek.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const ejs = require('ejs');
 const db = require('./db');
+const session = require('express-session');
+const pool = require('./db');
 
+
+
+//Törpe lista
 router.get('/', (req, res) => {
     db.query('SELECT * FROM torpek', (err, results) => {
-       console.log(results);
+       
+       if (err) {
+        return res.status(500).send('Database query error');
+        }
        ejs.renderFile('./views/index.ejs', { results }, (err, html) => {
         if (err) {
             res.status(500).send('Error rendering page');
@@ -13,12 +21,39 @@ router.get('/', (req, res) => {
             res.send(html);
         }
     });
-    }
-);
-
+    });
+});
+//Törpe hozzáadás Form
+router.get('/new', (req, res) => {
     
-
-   
-})
+    ejs.renderFile('./views/torpe-form.ejs', {session: req.session}, (err, html) => {
+    if (err) {
+      res.status(500).send('Error rendering page');
+    } else {
+    req.session.error = '';
+      res.send(html);
+    }
+    });
+    
+});
+//Törpe hozzáadás POST
+router.post('/new', (req, res) => {
+    const { nev,klan, nem, suly, magassag } = req.body;
+    if(nev =='' || klan =='' || Number(suly)==0 || Number(magassag) ==0 ||nem ==''){
+        req.session.error = 'Nem adtál meg minden adatot!'
+        req.session.severity = 'danger'
+        return res.redirect('/new')
+        
+    }
+    db.query('INSERT INTO torpek (nev, klan, nem, suly, magassag) VALUES (?, ?, ?, ?, ?)', [nev, klan, nem, suly, magassag], (err, result) => {
+        if (err) {
+            req.session.error = 'Hiba történt mentésnél!'
+            req.session.severity = 'danger'
+            return res.redirect('/new')
+        }
+        res.redirect('/');
+    });
+});
+//Törpe törlés
 
 module.exports = router;
